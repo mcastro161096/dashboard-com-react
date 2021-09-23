143f22fa0e3d2b171e7178c9cab79f285c8fa370
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-23 08:03:27 -0300

    Criado o componente Consultas, add na app,js.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import MenuSuperior from './Components/MenuSuperior';
 import Resumo from './Components/Resumo';
+import Consultas from './Components/Consultas';
 
 function App() {
   return (
     <div className="App">
       <MenuSuperior/>
       <Resumo/>
+      <Consultas/>
     </div>
   );
 }
