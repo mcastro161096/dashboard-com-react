a39e0d78ede2e5f394a85da275a8258567dd14dc
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-23 00:06:17 -0300

    Componente Resumo criado e importado no app.js.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import MenuSuperior from './Components/MenuSuperior';
+import Resumo from './Components/Resumo';
+
 function App() {
   return (
     <div className="App">
       <MenuSuperior/>
+      <Resumo/>
     </div>
   );
 }
