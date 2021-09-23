44e4bb8d7fa4729d5fe8a1420c5a60d31fd672cd
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-23 08:09:32 -0300

    Criando o componente Faturamento.

---
+++ src/Components/Faturamento/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class Faturamento extends Component{
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Faturamento;
