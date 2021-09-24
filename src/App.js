98a22271f4bc8d5324497f9177d270b164c10f4c
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-23 21:49:10 -0300

    Componente Faturamento concluído.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import MenuSuperior from './Components/MenuSuperior';
 import Resumo from './Components/Resumo';
 import Consultas from './Components/Consultas';
+import Faturamento from './Components/Faturamento';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <MenuSuperior/>
       <Resumo/>
       <Consultas/>
+      <Faturamento/>
     </div>
   );
 }
