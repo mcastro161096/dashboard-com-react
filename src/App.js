b51aa251b49240e6b7c5c3de69087ff4e6fc5961
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-23 22:22:46 -0300

    Add rotas distintas para cada componente.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import MenuSuperior from './Components/MenuSuperior';
 import Resumo from './Components/Resumo';
 import Consultas from './Components/Consultas';
 import Faturamento from './Components/Faturamento';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <MenuSuperior/>
-      <Resumo/>
-      <Consultas/>
-      <Faturamento/>
+      <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              <Switch>
+                <Route exact path="/" component={Resumo} />
+                <Route path="/consultas" component={Consultas} />
+                <Route path="/faturamento" component={Faturamento} />
+              </Switch>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
