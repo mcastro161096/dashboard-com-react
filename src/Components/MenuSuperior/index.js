2d0b46f8dd243bee6976189238eb1018571d8435
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-09-23 22:35:04 -0300

    Add Links para os componetes associados as rotas.

--- src/Components/MenuSuperior/index.js
+++ src/Components/MenuSuperior/index.js
@@ -1,16 +1,25 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
 
 class MenuSuperior extends Component {
     render(){
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a href="/" className="navbar-brand">Dash</a>
+                <Link href="/" className="navbar-brand">Dash</Link>
                 <button className="navbar-toggler" type="button" data-toggle="colapse" data-target="#navbarNav">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav mr-auto">
-
+                        <li className="nav-iten">
+                            <Link to="/" className="nav-link">Resumo</Link>
+                        </li>
+                        <li className="nav-iten">
+                            <Link to="/consultas" className="nav-link">Consultas</Link>
+                        </li>
+                        <li className="nav-iten">
+                            <Link to="/faturamento" className="nav-link">Faturamento</Link>
+                        </li>
                     </ul>
                 </div>
             </nav>
